b48998d739f616803a7223f129777e1db66ca55b
Akash Das <akashakki1100@gmail.com>
2026-03-24 10:50:44 +0530

    useragent bug fix

--- src/features/admin/hooks/useUsers.js
+++ src/features/admin/hooks/useUsers.js
@@ -6,9 +6,9 @@ import { MOCK_MODE, MOCK_USERS, fetchUsers } from "../api/usersApi";
 export default function useUsers() {
   return useQuery({
     queryKey: ["users"],
-    queryFn:  MOCK_MODE
+    queryFn: MOCK_MODE
       ? () => new Promise((res) => setTimeout(() => res(MOCK_USERS), 350))
-      : fetchUsers,
+      : () => fetchUsers({ roles: "admin,manager,agent", limit: 200 }),
     staleTime: 1000 * 60 * 5,
   });
 }
